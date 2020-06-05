c688cff3be211902bf0e6d9d0ffd4ee858a833a8
rafaazvd <rafa-spdf@hotmail.com>
2020-06-05 19:32:30 -0300

    initial commit

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -5,6 +5,14 @@ interface Balance {
   outcome: number;
   total: number;
 }
+interface CreateDTO {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+}
+interface Int {
+  value: number;
+}
 
 class TransactionsRepository {
   private transactions: Transaction[];
@@ -14,15 +22,45 @@ class TransactionsRepository {
   }
 
   public all(): Transaction[] {
-    // TODO
+    const list = this.transactions;
+    return list;
   }
 
   public getBalance(): Balance {
-    // TODO
+    const { income, outcome } = this.transactions.reduce(
+      (accumulator: Balance, transaction: Transaction) => {
+        switch (transaction.type) {
+          case 'income':
+            accumulator.income += transaction.value;
+            break;
+          case 'outcome':
+            accumulator.outcome += transaction.value;
+            break;
+          default:
+            break;
+        }
+
+        return accumulator;
+      },
+      {
+        income: 0,
+        outcome: 0,
+        total: 0,
+      },
+    );
+    const total = income - outcome;
+    const balance = {
+      income,
+      outcome,
+      total,
+    };
+    return balance;
   }
 
-  public create(): Transaction {
-    // TODO
+  public create({ title, value, type }: CreateDTO): Transaction {
+    const transaction = new Transaction({ title, value, type });
+    this.transactions.push(transaction);
+    return transaction;
   }
 }
 
